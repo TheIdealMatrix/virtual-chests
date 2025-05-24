a1f7d0fae7642b49f351d42cfe05796e0c489d9d
IdealMatrix <kivikplay2003@gmail.com>
2025-05-24 22:24:21 +0200

    Initial commit

---
+++ src/main/java/de/kevin_stefan/virtualChests/VirtualChests.java
@@ -0,0 +1,17 @@
+package de.kevin_stefan.virtualChests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VirtualChests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
