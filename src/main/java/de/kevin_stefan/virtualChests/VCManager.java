b51f426d5a6102c863ae4b0375b9bc5b1762cd79
Kevin Stefan <39440609+TheIdealMatrix@users.noreply.github.com>
2025-11-09 12:56:01 +0100

    Extract createVChestInventory & addHistory into own methods

--- src/main/java/de/kevin_stefan/virtualChests/VCManager.java
+++ src/main/java/de/kevin_stefan/virtualChests/VCManager.java
@@ -9,6 +9,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -25,20 +26,16 @@ public class VCManager {
     public static void openChest(Player player, OfflinePlayer targetPlayer, int number) {
         if (!openChests.containsKey(targetPlayer.getUniqueId())) {
             Component title = VirtualChests.getPluginLanguage().getFormatted(new Lang.CHEST_NAME(targetPlayer.getName(), number));
-            int size = VirtualChests.getPluginConfig().getInt("chest_rows") * 9;
-            Inventory inventory = Bukkit.createInventory(null, size, title);
 
+            Inventory inventory;
             VirtualChest virtualChest = StorageProvider.getInstance().getVChest(targetPlayer.getUniqueId(), number);
             if (virtualChest == null) {
                 virtualChest = new VirtualChest();
                 virtualChest.setPlayer(targetPlayer.getUniqueId());
                 virtualChest.setNumber(number);
+                inventory = createVChestInventory(title, null);
             } else {
-                ItemStack[] items = ItemStack.deserializeItemsFromBytes(virtualChest.getContent());
-                if (items.length > inventory.getSize()) {
-                    throw new RuntimeException(String.format("More items than space in chest. Did you lower chest_rows in config? (expected %d, got %d)", items.length / 9, inventory.getSize() / 9));
-                }
-                inventory.setContents(items);
+                inventory = createVChestInventory(title, virtualChest.getContent());
             }
 
             openChests.put(targetPlayer.getUniqueId(), new VCViewInfo(virtualChest, inventory));
@@ -71,29 +68,40 @@ public class VCManager {
             }
         }
 
-        // Save history
-        Bukkit.getScheduler().runTaskAsynchronously(VirtualChests.getInstance(), () -> {
-            UUID player = viewInfo.virtualChest.getPlayer();
-            Integer number = viewInfo.virtualChest.getNumber();
-
-            VirtualChestHistory newHistory = new VirtualChestHistory();
-            newHistory.setPlayer(player);
-            newHistory.setNumber(number);
-            newHistory.setContent(serializedItems);
-            newHistory.setTimestamp(System.currentTimeMillis());
-
-            VirtualChestHistory lastHistory = StorageProvider.getInstance().getLastVChestHistory(player, number);
-            if (lastHistory == null) {
-                StorageProvider.getInstance().addVChestHistory(newHistory);
-            } else if (!Arrays.equals(serializedItems, lastHistory.getContent())) {
-                StorageProvider.getInstance().addVChestHistory(newHistory);
-            }
-        });
+        addHistory(viewInfo.virtualChest.getPlayer(), viewInfo.virtualChest.getNumber(), serializedItems);
 
         if (inventory.getViewers().size() == 1) {
             // Only the player closing the inventory is viewing
             openChests.remove(viewInfo.virtualChest.getPlayer());
         }
+    }
+
+    public static Inventory createVChestInventory(Component title, @Nullable byte[] content) {
+        int size = VirtualChests.getPluginConfig().getInt("chest_rows") * 9;
+        Inventory inventory = Bukkit.createInventory(null, size, title);
+        if (content != null) {
+            ItemStack[] items = ItemStack.deserializeItemsFromBytes(content);
+            if (items.length > inventory.getSize()) {
+                throw new RuntimeException(String.format("More items than space in chest. Did you lower chest_rows in config? (expected %d, got %d)", items.length / 9, inventory.getSize() / 9));
+            }
+            inventory.setContents(items);
+        }
+        return inventory;
+    }
+
+    public static void addHistory(UUID player, int number, byte[] content) {
+        Bukkit.getScheduler().runTaskAsynchronously(VirtualChests.getInstance(), () -> {
+            VirtualChestHistory newHistory = new VirtualChestHistory();
+            newHistory.setPlayer(player);
+            newHistory.setNumber(number);
+            newHistory.setContent(content);
+            newHistory.setTimestamp(System.currentTimeMillis());
+
+            VirtualChestHistory lastHistory = StorageProvider.getInstance().getLastVChestHistory(player, number);
+            if (lastHistory == null || !Arrays.equals(content, lastHistory.getContent())) {
+                StorageProvider.getInstance().addVChestHistory(newHistory);
+            }
+        });
     }
 
 }
